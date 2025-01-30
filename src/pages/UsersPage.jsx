a4a5e96346167367904424ac03d72df48941a9c9
“Tayyab <tayyabhaseeb5@gmail.com>
2025-01-30 15:44:07 +0000

    shimmer

--- src/pages/UsersPage.jsx
+++ src/pages/UsersPage.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { getAllUsers } from "../api/api";
-import Spinner from "../components/otherComponents/Spinner";
+
 import UsersCard from "../components/usersComponents/usersCard";
+import Shimmer from "../components/Shimmer/Shimmer";
 
 function UsersPage() {
   const [users, setUsers] = useState([]);
@@ -26,7 +27,7 @@ function UsersPage() {
   return (
     <div className=" my-6 p-4 flex flex-col  items-center md:flex md:flex-row md:flex-wrap md:justify-center md:gap-4">
       {loading ? (
-        <Spinner />
+        <Shimmer />
       ) : (
         <>
           {users.map((obj) => {

5db98287bdf083037100207f69c2b7a30add01e7
“Tayyab <tayyabhaseeb5@gmail.com>
2025-01-31 20:34:24 +0000

    avatar

--- src/components/HeaderComponents/Avatar.jsx
+++ src/components/HeaderComponents/Avatar.jsx
@@ -4,7 +4,7 @@ function Avatar() {
   return (
     <img
       className="w-10 h-10 p-1 rounded-full ring-2 ring-[rgba(235,27,36)] dark:ring-gray-500 md:w-16 md:h-16"
-      src="https://img.freepik.com/free-vector/smiling-young-man-illustration_1308-174669.jpg"
+      src="https://vignette.wikia.nocookie.net/mrmen/images/7/78/Mr-Grumpy-3A.PNG/revision/latest?cb=20170707233013"
       alt="Bordered avatar"
     />
   );
